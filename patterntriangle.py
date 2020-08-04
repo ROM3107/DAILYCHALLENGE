cd33805cd72596c751274e7364b9ccf9ad311d18
Roma Dey <56331079+ROM3107@users.noreply.github.com>
2020-08-04 21:37:32 +0530

    Triangle pattern

---
+++ patterntriangle.py
@@ -0,0 +1,17 @@
+def printPattern(n): 
+    for i in range(1, n + 1):
+        for j in range(i, n):
+            print("\t", end = "") 
+  
+        t = i 
+  
+        
+        for k in range (1, i + 1): 
+            print(t, "\t", "\t", end = "") 
+            t = t + n - k 
+          
+        print() 
+      
+
+n = 5
+printPattern(n) 
